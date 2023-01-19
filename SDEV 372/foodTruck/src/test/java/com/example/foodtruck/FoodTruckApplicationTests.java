c4b1e3c1fb930c2672987ccffc3f0f717f26ac57
Xavier <Denson.Xavier@student.greenriver.edu>
2023-01-19 09:27:41 -0800

    initial commit

---
+++ SDEV 372/foodTruck/src/test/java/com/example/foodtruck/FoodTruckApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.foodtruck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodTruckApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
