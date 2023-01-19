efa42a94bbf9949db1dfaa3268c81f44be39b956
Xavier <Denson.Xavier@student.greenriver.edu>
2023-01-19 10:13:21 -0800

    initial commit

---
+++ 372/src/test/java/edu/greenriver/foodtruck/FoodtruckApplicationTests.java
@@ -0,0 +1,15 @@
+package edu.greenriver.foodtruck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodtruckApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
