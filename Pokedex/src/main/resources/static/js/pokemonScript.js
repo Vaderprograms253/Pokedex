0e1b46e053686476302aa5ee64fc26b72736d34d
Xavier <Denson.Xavier@student.greenriver.edu>
2023-08-30 13:41:40 -0700

    Added datatable and populated with pokemon api

--- Pokedex/src/main/resources/static/js/pokemonScript.js
+++ Pokedex/src/main/resources/static/js/pokemonScript.js
@@ -1,9 +1,13 @@
 window.onload = function ()
 {
     load();
+
 }
 
+
+
 let pokemonTotal;
+let count = 1;
 
 async function load()
 {
@@ -26,13 +30,46 @@ function showPokemon(pokemon)
 {
     let pokemonList = pokemon.results;
     pokemonTotal = pokemon.count;
-    for (let i = 0; i < pokemonList.length; i++){
+    console.log(pokemonTotal);
+    for (let i = 0; i < pokemon.count; i++){
         let nextPokemon = pokemonList[i];
         addNewPokemon(nextPokemon);
+        count++;
     }
 }
 
 function addNewPokemon(pokemon)
 {
-    console.log(pokemon.name);
+    let list = document.getElementById("pokeTableBody");
+    let name = pokemon.name;
+    let pokemonUrl = pokemon.url;
+    let tr = document.createElement("tr");
+    let td = document.createElement("td");
+
+    tr.setAttribute("id", "tr-" +  name);
+    td.setAttribute("id", "td-name-" + name);
+    name = capitalizeFirstLetter(name);
+    td.innerHTML = name;
+    tr.appendChild(td);
+    list.appendChild(tr);
+
+    displayDataTable();
+
+}
+
+function capitalizeFirstLetter(pokeString)
+{
+    return pokeString.charAt(0).toUpperCase() + pokeString.slice(1);
+}
+
+function displayDataTable()
+{
+    console.log("count = " + count);
+    console.log("total = " + pokemonTotal);
+    if (count === pokemonTotal)
+    {
+        $(document).ready(function () {
+            $("#pokeTable").DataTable();
+        });
+    }
 }
